a320ce9ef34cc436a8d1018757804f5ef6691958
Aadil Glasswala <aglasswala@gmail.com>
2018-09-26 17:15:51 -0400

    Changed up the users

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import CardList from './CardList';
 import SearchBox from './SearchBox';
-import { robots } from './robots';
 import './App.css';
 
 class App extends React.Component {
@@ -14,7 +13,10 @@ class App extends React.Component {
 	}
 
 	componentDidMount() {
-		this.setState({ robots: robots})
+		fetch('https://jsonplaceholder.typicode.com/users')
+			.then(response => response.json())
+			.then(users => this.setState({ robots: users}));
+
 	}
 
 	onSearchChange = (event) => {
