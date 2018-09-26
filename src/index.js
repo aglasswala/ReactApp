c4bc7fc17e422d07bd2cf9db0cf12eac58fe954b
Aadil Glasswala <aglasswala@gmail.com>
2018-09-26 12:29:24 -0400

    Added more cards and cleaned up code

--- src/index.js
+++ src/index.js
@@ -1,19 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Card from './Card.js';
+import CardList from './CardList';
 import registerServiceWorker from './registerServiceWorker';
 import 'tachyons';
+import { robots } from './robots'
 
 ReactDOM.render(
-				<div>
-					<Card />
-					<Card />
-					<Card />
-				</div>
-
-
-
-
-				, document.getElementById('root'));
+				<CardList robots={robots} />
+, document.getElementById('root'));
 registerServiceWorker();
