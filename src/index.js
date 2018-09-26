2220e934d9d49f6e5a83b9fa4a2eb195fd476582
Aadil Glasswala <aglasswala@gmail.com>
2018-09-26 12:02:49 -0400

    initial commit

---
+++ src/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import Card from './Card.js';
+import registerServiceWorker from './registerServiceWorker';
+import 'tachyons';
+
+ReactDOM.render(
+				<div>
+					<Card />
+					<Card />
+					<Card />
+				</div>
+
+
+
+
+				, document.getElementById('root'));
+registerServiceWorker();
