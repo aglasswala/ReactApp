60e177d653895957ab75a0c41a696f5080affbc3
Aadil Glasswala <aglasswala@gmail.com>
2018-09-26 17:34:44 -0400

    Added Scrolling Feature

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import CardList from './CardList';
 import SearchBox from './SearchBox';
+import Scroll from './Scroll';
 import './App.css';
 
 class App extends React.Component {
@@ -31,7 +32,9 @@ class App extends React.Component {
 			<div className="tc">
 				<h1 className="f2">RoboFriends</h1>
 				<SearchBox searchChange={this.onSearchChange}/>
-				<CardList robots ={ filteredRobots }/>
+				<Scroll>
+					<CardList robots ={ filteredRobots }/>
+				</Scroll>
 			</div>
 		);
 	}
